2343f5c0eb8e3de145ba14bf59a9cdb8f9ff836b
João Marcos <jm.cibersec@gmail.com>
2026-05-08 18:29:59 -0300

    Update eventos.py

--- src/eventos.py
+++ src/eventos.py
@@ -7,7 +7,7 @@ def cadastrar_evento():
 
     print("\nCadastro de Evento")
 
-    nome = input("Nome: ")
-    tipo = input("Tipo: ")
-    data = input("Data (dd/mm/aaaa): ")
-    local = input("Local: ")
+    nome = input("Nome do evento: ")
+    tipo = input("Tipo do evento: ")
+    data = input("Data do evento (dd/mm/aaaa): ")
+    local = input("Local do evento: ")
