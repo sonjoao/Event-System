1c2a5fd15c3e7732f4f53b0f85fd7fff5e33edf9
Miguel B. Cavalcanti <mbc4@cesar.school>
2026-05-21 13:14:20 -0300

    Update tarefas.py
    busca de evento antes do cadastro, e entrada de dados.
    - próximo passo: entrada de custos, (uso de Números).

--- src/tarefas.py
+++ src/tarefas.py
@@ -12,3 +12,8 @@ def criar_tarefa():
   print("\n--- CADASTRO DE TAREFA ---") #objetivo: listar todos os eventos para o user saber qual evento colocar a tarefa
   listar_eventos()
   id_evento = input("\nDigite o id do evento dessa tarefa: ")
+  if buscar_evento_por_id(id_evento) is None: #variavel pra ver se o evento existe antes de cadastrar a tarefa 
+    print("Evento não encontrado. Cadastre um evento válido.")
+    return
+id_tarefa = gerar_id_tarefa()
+nome_tarefa = input("Nome da tarefa: ")
