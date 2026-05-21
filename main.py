c603c1aca8947e10733e0307263aa40bf2a0453c
João Marcos <jm.cibersec@gmail.com>
2026-05-21 19:38:18 -0300

    Update main.py - jm
    refazendo os prints

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
-from src.eventos import *
-from src.tarefas import *
-from src.orcamento import *
-from src.susgestoes import *
+from src.eventos import criar_evento, listar_eventos, editar_evento, excluir_evento
+from src.tarefas import criar_tarefa, listar_tarefas, listar_tarefas_por_evento, concluir_tarefa, excluir_tarefa
+from src.orcamento import mostrar_orcamento
+from src.sugestoes import mostrar_contagem_regressiva, mostrar_sugestoes
 
-def menu_eventos():
+def menu_eventos
   while True:
-    print("\n========= SISTEMAS  DE EVENTOS ===========)
-    print("1 - Eventos")
-    print("2 - Tarefas")
-    print("3 - Orçamentos")
-    print("4 - Sugestões")
-    print("0 - Sair")
+    print("\n ========= MENU DE EVENTOS =========)
+    print("1 - Cadastrar evento")
 
-    opcao = input("\nEscolha uma opção: ")
-
-    if opcao == "1":
-        print("\nAbrindo eventos...")
-        menu_eventos()
-    elif opcao == "2"
-        print("\nAbrindo tarefas...")
-        menu_tarefas()
-    elif opcao == "3"
-        print("\nAbrindo orçamentos...")
-        menu_orcamentos()
-    elif opcao == "4"
-        print("\nAbrindo susgestões...")
-        menu_susgestoes()
-    elif opcao == "0"
-        print("\nSistema encerrado.")
-        break
     
     else:
       print("\nOpção inválida! Tente novamente.")
