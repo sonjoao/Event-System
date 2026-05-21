b0627ca81213d6c22ab5c16e448d07f73fc7442a
João Marcos <jm.cibersec@gmail.com>
2026-05-21 18:46:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from src.eventos import *
+from src.tarefas import *
+from src.orcamento import *
+from src.susgestoes import *
+
+def menu():
+  while True:
+    print("\n========= SISTEMAS  DE EVENTOS ===========)
+    print("1 - Eventos")
+    print("2 - Tarefas")
+    print("3 - Orçamentos")
+    print("4 - Sugestões")
+    print("0 - Sair")
