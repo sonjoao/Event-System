58810a16f8f9617ef1083c8ad142df9fd7f5c9c9
João Marcos <jm.cibersec@gmail.com>
2026-05-29 20:50:22 -0300

    Update auth.py
    vamos começar uma página de login, primeira estabeleci o modulo json

--- src/auth.py
+++ src/auth.py
@@ -1,1 +1,9 @@
+import json
+ARQUIVO = "dados/usuarios.jason"
 
+def carregar_usuarios():
+    try:
+        with open(ARQUIVO, "r") as arquivo:
+            return json.load(arquivo)
+    except:
+        return []
