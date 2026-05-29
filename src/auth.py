c55bb0ef8224f4039aa017d5d7f57419c26b5f96
João Marcos <jm.cibersec@gmail.com>
2026-05-29 20:25:09 -0300

    Update auth.py

--- src/auth.py
+++ src/auth.py
@@ -3,4 +3,12 @@ import json
 import os
 
 ARQUIVO_USUARIOS = "dados/usuarios.json"
+#tive que recorrer a claude para me ensinar como fazer
 
+def hash_senha(senha):
+    return hashlib.sha256(senha.encode()).hexdigest()
+
+def carregar_usuarios():
+    if not os.path.exists(ARQUIVOS_USUARIOS):
+      return []
+    with open(ARQUIVO_USUARIOS, "r", encoding="utf-8) as f:
