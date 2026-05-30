bb2da1abcbdac3dbd1920647c6ad071c728fb4ab
João Marcos <jm.cibersec@gmail.com>
2026-05-30 10:26:54 -0300

    Update main.py - importando a parte da galera

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-from src.eventos import criar_evento, listar_eventos, editar_evento, excluir_evento
-from src.tarefas import criar_tarefa, listar_tarefas, listar_tarefas_por_evento, concluir_tarefa, excluir_tarefa
+from src.auth import tela_inicial
+
+from src.eventos import (
+  criar_evento
+  listar_eventos
+  editar_evento,
+  excluir_evento
+)
+
+from src.tarefas import (
+  criar_tarefa,
+  listar_tarefa,
+  listar_tarefas_por_evento,
+  concluir_tarefa,
+  excluir_tarefa
+)
+
 from src.orcamento import mostrar_orcamento
-from src.sugestoes import mostrar_contagem_regressiva, mostrar_sugestoes
+from src.susgestoes import mostrar_contagem_regressiva, mostrar_sugestoes
 
-
-                
-              
-              
-
-         
-        
-    
   
-    
-    
-    
-
-
