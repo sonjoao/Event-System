d9f41f406bf04d833f8f94c7d9666d9bc05eb736
Britto16-hash <vbcvs@cesar.school>
2026-06-03 16:06:09 -0300

    Update Extras.py

--- src/Extras.py
+++ src/Extras.py
@@ -1,1 +1,11 @@
-
+from datetime import datetime
+def verificar_dia_evento(data_evento):
+  hoje=datetime.today()
+  data=datetime.strptime(
+      data_evento,
+      "%d/%m/%Y"
+  )
+  dias=(data-hoje).days
+  return dias
+  
+  
