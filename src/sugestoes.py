6bb070a159880a9ceeabb56b242f82047fb2778f
João Marcos <jm.cibersec@gmail.com>
2026-06-03 16:07:41 -0300

    Update sugestoes.py
    corrigindo

--- src/sugestoes.py
+++ src/sugestoes.py
@@ -1,7 +1,3 @@
-from src.auth import tela_inicial
-
-from src.eventos import (
-    criar_evento,
-    listar_eventos,
-    editar_evento
-    excluir_evento
+from datetime import datetime
+from src.eventos import listar_eventos, buscar_evento_por_id
+from src.orcamento import calcular_gastos_evento
