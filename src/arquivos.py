9873bc32a38da8b1f4bc57a5271ec6ade41aa908
João Marcos <jm.cibersec@gmail.com>
2026-06-03 18:15:51 -0300

    Update arquivos.py

--- src/arquivos.py
+++ src/arquivos.py
@@ -2,6 +2,29 @@ def ler_arquivo(caminho):
   linhas = []
 
 try:
-  with open(caminho, linhas):
+  with open(caminho, "r", encondig="utf-8") as arquivo:
+    for linha in arquivo:
+        linha = linha.strip()
 
-    with open(caminho, "W" ,  enconding="utf-8") as arquivo
+        if linha != "":
+         linhas.append(linha)
+
+  except FileNotFoundError:
+    return []
+ return linhas
+
+def salvar_arquivo(caminho, linhas):
+  with open(caminho, "w", encoding="utf-8") as arquivo:
+    for linha in linhas:
+      arquivo.write(linha + "\n")
+
+def adicionar_linha(caminho, linha):
+  with open(caminho, "a", encondig="utf-8) as arquivo:
+            arquivo.write(linha + "\n")
+
+
+
+
+
+
+
