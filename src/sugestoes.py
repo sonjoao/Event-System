85c62d41e975281a231026a65894d4f988a43af9
João Marcos <jm.cibersec@gmail.com>
2026-06-03 16:15:32 -0300

    Update sugestoes.py

--- src/sugestoes.py
+++ src/sugestoes.py
@@ -1,3 +1,31 @@
 from datetime import datetime
 from src.eventos import listar_eventos, buscar_evento_por_id
 from src.orcamento import calcular_gastos_evento
+
+def mostar_contagem_regressiva():
+    print("\n=== Contagem Regressiva ===")
+
+    listar_eventos()
+
+    id_evento = input("\nDigite o ID do evento: ")
+    evento = buscar_evento_por_id(id_evento)
+
+    if evento is None:
+        print(Evento não encontrado. ")
+        return
+
+      dados = evento.split(";")
+
+      data_evento = datetime.strptime(dados[3], "%d/%m/%Y)
+
+      hoje = datetime.now()
+      dias_restantes = (data_eventos - hoje).days
+
+      
+
+
+                                    
+
+
+                                
+      
